b84810b0eb65028eb0efc55c26b45ca338635e18
Anduamlak Alehegne <anduamlakalehegne@gmail.com>
2024-12-23 22:31:38 +0300

    .

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,31 +1,38 @@
-import mongoose from 'mongoose';
+import mongoose, { Connection } from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI as string;
 
 if (!MONGODB_URI) {
   throw new Error(
     'Please define the MONGODB_URI environment variable inside .env.local'
-  )
+  );
+}
+
+declare global {
+  var mongoose: { conn: Connection | null; promise: Promise<Connection> | null };
 }
 
 let cached = global.mongoose;
 
+// Initialize the cache if it doesn't exist
 if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
 }
 
 async function dbConnect() {
+  // If there's already an existing connection, return it
   if (cached.conn) {
     return cached.conn;
   }
 
+  // Otherwise, establish a new connection if there's no promise in progress
   if (!cached.promise) {
     const opts = {
-      bufferCommands: false,
+      bufferCommands: false, // Disable buffering of commands until the connection is established
     };
 
     cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-      return mongoose;
+      return mongoose.connection;
     });
   }
 
@@ -40,4 +47,3 @@ async function dbConnect() {
 }
 
 export default dbConnect;
-
